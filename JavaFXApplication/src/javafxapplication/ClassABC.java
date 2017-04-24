21918136f32d84908a714eb20d76d30b3b7a00b9
Jdongju <dj9110@naver.com>
2017-04-24 15:38:22 +0900

    테슷흐

--- JavaFXApplication/src/javafxapplication/ClassABC.java
+++ JavaFXApplication/src/javafxapplication/ClassABC.java
@@ -2,7 +2,7 @@ package javafxapplication;
 
 public class ClassABC {
     public static void main(String[] args){
-    System.out.println("싸우지마요");
+    System.out.println("싸우지마요2");
     }
     
 }
