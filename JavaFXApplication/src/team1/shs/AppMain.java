fe82fbaeee610b8adc21c4c383909e186ac244e5
Marsseo <ghktjddl3@naver.com>
2017-04-25 18:15:14 +0900

    나만의 메뉴 완성

---
+++ JavaFXApplication/src/team1/shs/AppMain.java
@@ -0,0 +1,30 @@
+package team1.shs;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("root2.fxml"));
+			Scene scene = new Scene(parent);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Team Project");
+			primaryStage.show();
+		} catch (IOException ex) {
+			System.err.println("예외 발생");
+		}
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
+}
