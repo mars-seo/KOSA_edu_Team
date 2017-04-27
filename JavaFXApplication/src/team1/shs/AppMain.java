43fb6e419e21bf382d39a5b5091199f2e5047e6b
Marsseo <ghktjddl3@naver.com>
2017-04-28 00:27:42 +0900

    신메뉴 완성

--- JavaFXApplication/src/team1/shs/AppMain.java
+++ JavaFXApplication/src/team1/shs/AppMain.java
@@ -12,7 +12,7 @@ public class AppMain extends Application{
     @Override
     public void start(Stage primaryStage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("root2.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getResource("newMenu.fxml"));
 			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Team Project");
