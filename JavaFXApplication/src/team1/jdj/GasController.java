332cb863ef06308a3c29591bb97db69f3ac9386a
Jdongju <dj9110@naver.com>
2017-04-30 13:12:32 +0900

    ff

--- JavaFXApplication/src/team1/jdj/GasController.java
+++ JavaFXApplication/src/team1/jdj/GasController.java
@@ -19,6 +19,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import team1.jdj.Fee;
 
 public class GasController implements Initializable {
 
