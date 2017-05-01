5e9c0bb638900996516aac5d56ce6f6ee4e9368d
Marsseo <ghktjddl3@naver.com>
2017-05-01 22:38:51 +0900

    미디어 플레이어로 수정 및 버튼 추가

---
+++ JavaFXApplication/src/team1/shs/MiniMediaController.java
@@ -0,0 +1,48 @@
+package team1.shs;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.MediaView;
+
+
+public class MiniMediaController implements Initializable {
+
+	@FXML
+	private Button previousBtn;
+	@FXML
+	private Button pauseBtn;
+	@FXML
+	private Button playBtn;
+	@FXML
+	private Button stopBtn;
+	@FXML
+	private Button nextBtn;
+	@FXML
+	private Button uploadBtn;
+	@FXML
+	private ImageView imgSound;
+	@FXML
+	private Slider volumeSlider;
+	@FXML
+	private Slider mediaSlider;
+	@FXML
+	private Label playTime;
+	@FXML
+	private MediaView media;
+	@FXML
+	private ListView<?> playList;
+
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}	
+	
+}
