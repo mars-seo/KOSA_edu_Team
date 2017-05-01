3fbe755b27a8488f73132c2286f5af2a9a18b579
Jdongju <dj9110@naver.com>
2017-05-02 00:53:52 +0900

    hell

---
+++ JavaFXApplication/src/team1/jdj/GasController.java
@@ -0,0 +1,97 @@
+package team1.jdj;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+public class GasController implements Initializable {
+
+    @FXML
+    private Button chartButton;
+    @FXML
+    private TableView<Fee> tableView;
+    @FXML
+    private Label howManyUseLabel;
+    @FXML
+    private Label howManyFeeLabel;
+
+    @FXML
+    private BorderPane BoaderPane;
+    @FXML
+    private StackPane mainStackPane;
+
+    private static StackPane rootPane;
+    @FXML
+    private AnchorPane gasMainAnchorPane;
+    @FXML
+    private AnchorPane buttonAnchorPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        rootPane = mainStackPane;
+        TableColumn tc0 = tableView.getColumns().get(0);
+        TableColumn tc1 = tableView.getColumns().get(1);
+        TableColumn tc2 = tableView.getColumns().get(2);
+        TableColumn tc3 = tableView.getColumns().get(3);
+
+        tc0.setCellValueFactory(new PropertyValueFactory<Fee, String>("month"));
+        tc1.setCellValueFactory(new PropertyValueFactory<Fee, String>("howManyUse"));
+        tc2.setCellValueFactory(new PropertyValueFactory<Fee, String>("fee"));
+        tc3.setCellValueFactory(new PropertyValueFactory<Fee, String>("check"));
+
+        tc0.setStyle("-fx-alignment: CENTER;");
+        tc1.setStyle("-fx-alignment: CENTER;");
+        tc2.setStyle("-fx-alignment: CENTER;");
+        tc3.setStyle("-fx-alignment: CENTER;");
+
+        ObservableList<Fee> list = FXCollections.observableArrayList();
+        LocalDateTime now = LocalDateTime.now();
+        int DateDay = now.getDayOfMonth();
+        int Hour = now.getHour();
+        int Minute = now.getMinute();
+        int Second = now.getSecond();
+        int feeNumber = (DateDay * 24 * 60 * 60 + Hour * 60 * 60 + Minute * 60 + Second) / 20000;
+        String feeMeter = String.valueOf(feeNumber) + "\u33A5";
+        String fee = String.valueOf(feeNumber * 300) + "원";
+        list.add(new Fee("1", "128" + "\u33A5", "69120원", "납부완료"));
+        list.add(new Fee("2", "111" + "\u33A5", "59940원", "납부완료"));
+        list.add(new Fee("3", "78" + "\u33A5", "42120원", "납부완료"));
+
+        tableView.setItems(list);
+
+        howManyUseLabel.setText(feeMeter);
+        howManyFeeLabel.setText(fee);
+
+        chartButton.setOnAction((event) -> {
+            handlerChartButton(event);
+        });
+    }
+
+    private void handlerChartButton(ActionEvent event) {
+        try {
+            
+            rootPane.getChildren().clear();
+            Parent gasScene = FXMLLoader.load(getClass().getResource("gasChart.fxml"));
+            rootPane.getChildren().add(gasScene);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
---
+++ JavaFXApplication/src/team1/jdj/RootController.java
@@ -0,0 +1,100 @@
+package team1.jdj;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class RootController implements Initializable {
+
+    @FXML
+    private Button btnGasFee;
+    @FXML
+    private Button btnElectricFee;
+    @FXML
+    private Button btnWaterFee;
+
+    private static StackPane rootPane;
+    @FXML
+    private StackPane changeableStackPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        rootPane = changeableStackPane;
+
+        btnGasFee.setOnAction((event) -> {
+            handlerBtnGasFee(event);
+        });
+        btnElectricFee.setOnAction((event) -> {
+            handlerBtnElectricFee(event);
+        });
+        btnWaterFee.setOnAction((event) -> {
+            handlerBtnWaterFee(event);
+        });
+    }
+
+    public RootController() {
+
+    }
+
+    private void handlerBtnGasFee(ActionEvent event) {
+        try {
+            rootPane.getChildren().clear();
+
+            Parent scene = FXMLLoader.load(getClass().getResource("gas.fxml"));
+            rootPane.getChildren().add(scene);
+            scene.setTranslateX(840);
+            Timeline timeline = new Timeline();
+            KeyValue keyValue = new KeyValue(scene.translateXProperty(), 0);
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(500), keyValue);
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.play();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void handlerBtnElectricFee(ActionEvent event) {
+        try {
+            rootPane.getChildren().clear();
+            Parent scene = FXMLLoader.load(getClass().getResource("electric.fxml"));
+            rootPane.getChildren().add(scene);
+            scene.setTranslateX(840);
+            Timeline timeline = new Timeline();
+            KeyValue keyValue = new KeyValue(scene.translateXProperty(), 0);
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(500), keyValue);
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.play();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void handlerBtnWaterFee(ActionEvent event) {
+        try {
+            rootPane.getChildren().clear();
+            Parent scene = FXMLLoader.load(getClass().getResource("water.fxml"));
+            rootPane.getChildren().add(scene);
+
+            scene.setTranslateX(840);
+            Timeline timeline = new Timeline();
+            KeyValue keyValue = new KeyValue(scene.translateXProperty(), 0);
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(500), keyValue);
+            timeline.getKeyFrames().add(keyFrame);
+            timeline.play();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
