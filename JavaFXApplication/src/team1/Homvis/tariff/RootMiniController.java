7f601e02da12b559589b59e2531c7fe0958ba027
Jdongju <dj9110@naver.com>
2017-05-10 14:02:54 +0900

    water image added

--- JavaFXApplication/src/team1/Homvis/tariff/RootMiniController.java
+++ JavaFXApplication/src/team1/Homvis/tariff/RootMiniController.java
@@ -105,7 +105,7 @@ public class RootMiniController implements Initializable {
         } else if (num == 1) {
             btnElectricFee.setImage(new Image(getClass().getResource("tariffImg/elecNgas_lighticon_on.png").toString()));
         } else if (num == 2) {
-            btnWaterFee.setImage(new Image(getClass().getResource("tariffImg/boiler_hotwater_clicked.png").toString()));
+            btnWaterFee.setImage(new Image(getClass().getResource("tariffImg/elecNgas_water_clicked.png").toString()));
         }
     }
 
